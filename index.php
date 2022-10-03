bd8220e0808697396e784eba5c653cd0eda96956
Mursalin <[mursalinxz1@gmail.com]>
2022-10-03 23:32:47 +0600

    updated login

--- index.php
+++ index.php
@@ -1,106 +1,90 @@
-<?php
-    session_start();
-    include('config/database.php');
-    include('config/tasks.php');
-    $obj = new Tasks;
-    
-    if(isset($_POST['submit'])) {
-        $task = $_POST['task'];
-        $id = $_POST['id'];
-        $created_at = $updated_at = date("Y-m-d H:i:s");
-
-        if(!empty($id)) {
-            $sql = "UPDATE todolists set task = '".$task."', updated_at = '".$updated_at."' where id = ".$id;
-            $res = $obj->executeQuery($sql);
-            if($res) {
-                $_SESSION['success'] = "Task has been update successfully";
-            }
-            else {
-                $_SESSION['error'] = "Something went wrong, please try again later";
-            }
-        }   
-        else {
-            $sql = "INSERT INTO todolists (task, created_at, updated_at) VALUES ('".$task."', '".$created_at."', '".$updated_at."')";
-            $res = $obj->executeQuery($sql);
-
-            if($res) {
-                $_SESSION['success'] = "Task has been created successfully";
-            }
-            else {
-                $_SESSION['error'] = "Something went wrong, please try again later";
-            }
-        }
-        
-        session_write_close();
-        header("LOCATION:index.php");
-    }
-
-    //Get all Tasks
-    $tasks = $obj->getAllTasks();
-
-    //Get Task
-    $editing = false;
-    if(isset($_GET['action']) && $_GET['action']  === 'edit') {
-        $taskData = $obj->getTask($_GET['id']);
-        $editing = true;
-    }
-
-    //Delete Task
-    if(isset($_GET['action']) && $_GET['action']  === 'delete') {
-        $sql = "DELETE FROM todolists WHERE id = ".$_GET['id'];
-        $res = $obj->executeQuery($sql);
-        if($res) {
-            $_SESSION['success'] = "Task has been deleted successfully";
-        }
-        else {
-            $_SESSION['error'] = "Something went wrong, please try again later";
-        }
-
-        session_write_close();
-        header("LOCATION:index.php");
-    }
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="../../css/assets/fontawesome/css/all.css" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/css/style.css"/>
-    <title>Todo List </title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Simple Login Form with Blue Background</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            color: #fff;
+            background: #3598dc;
+            background: #a94442;
+        }
+        .form-control {
+            min-height: 41px;
+            background: #f2f2f2;
+            box-shadow: none !important;
+            border: transparent;
+        }
+        .form-control:focus {
+            background: #e2e2e2;
+        }
+        .form-control, .btn {
+            border-radius: 2px;
+        }
+        .login-form {
+            width: 350px;
+            margin: 250px auto;
+            text-align: center;
+        }
+        .login-form h2 {
+            margin: 10px 0 25px;
+        }
+        .login-form form {
+            color: #7a7a7a;
+            border-radius: 3px;
+            margin-bottom: 15px;
+            background: #fff;
+            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+            padding: 30px;
+        }
+        .login-form .btn {
+            font-size: 16px;
+            font-weight: bold;
+            background: #3598dc;
+            border: none;
+            outline: none !important;
+        }
+        .login-form .btn:hover, .login-form .btn:focus {
+            background: #2389cd;
+        }
+        .login-form a {
+            color: #fff;
+            text-decoration: underline;
+        }
+        .login-form a:hover {
+            text-decoration: none;
+        }
+        .login-form form a {
+            color: #7a7a7a;
+            text-decoration: none;
+        }
+        .login-form form a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
-
-    <div class="container">
-      <div id="newtask">
-        <?php include('include/alert.php') ?>
-
-
-        <h3>Todo List App</h3>
-        <form action="index.php" method="post" id="taskform">
-            <input type="hidden" name="id" value="<?php if($editing) { echo $taskData['id']; } ?>"  >
-            <input type="text" name="task" id="task" placeholder="Task to be done..." value="<?php if($editing) { echo $taskData['task']; } ?>" />
-            <button type="submit" name="submit" id="add"><?php if($editing) { echo "Update"; } else { echo "Add" ; } ?></button>
-        </form>
-      </div>
-
-      <div id="tasks">
-        <?php
-            if(!empty($tasks)) {
-                foreach($tasks as $t) {
-        ?>
-        <div class="task">
-            <span><?php echo $t['task'] ?></span>
-            <a href="index.php?action=edit&id=<?php echo $t['id'] ?>" class="edit button">Edit<i class="fa fa-edit"></i></a>
-            <a onclick="return confirm('Do you want to delete this record?')" href="index.php?action=delete&id=<?php echo $t['id'] ?>" class="delete button">Del<i class="fa fa-trash-alt"></i></a>
+<div class="login-form">
+    <form action="/front.php" method="get">
+        <h2 class="text-center">Login</h2>
+        <div class="form-group has-error">
+            <input type="text" class="form-control" name="username" placeholder="Username" required="required">
         </div>
-        <?php }} ?>
-      </div>
-    </div>
+        <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-lg btn-block" name="login-button" >Sign in</button>
+        </div>
+    </form>
+    <!--    <p class="text-center small">Don't have an account? <a href="#">Sign up here!</a></p>-->
+</div>
 
-    <script src="assets/js/app.js"></script>
 </body>
 </html>
